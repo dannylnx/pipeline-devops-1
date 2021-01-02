0a420a405284d601e1d25fbc82cf1d1cb001fbed
rpinoga <34929141+rpinoga@users.noreply.github.com>
2021-01-02 20:39:04 -0300

    Update ejecucion.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -1,25 +1,19 @@
 def call(){
-
     pipeline {
         agent any
-
-        parameters { 
-            choice(name: 'buildtool', choices: ['gradle','maven'], description: 'Elección de herramienta de construcción para aplicación covid')
-            string(name: 'stages', defaultValue: '' , description: 'Escribir stages a ejecutar en formato: stage1;stage2;stage3. Si stage es vacío, se ejecutarán todos los stages.') 
-        }
-
+        
         stages {
             stage('Pipeline') {
                 steps {
                     script{
-                        println 'Herramienta de ejecución seleccionada: ' + params.buildtool
-
-                        if (params.buildtool == 'gradle'){
-                            gradle "${params.stages}" 
-                        } else {
-                            maven "${params.stages}"
+                        figlet params.buildtool
+                        stages.split(',').each{
+                            try{
+                                "${params.buildtool}"."${it}"()
+                            }catch (Exception e){
+                                error "Error en stage ${it}: ${e}"
+                            }
                         }
-
                     }
                 }
             }
