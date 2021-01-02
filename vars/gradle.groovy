2093aeb50e03a3225341d81d74299bd5f563efc6
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2021-01-02 19:26:51 -0300

    cambio run por jar en gradle

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -7,7 +7,7 @@ def call(String chosenStages){
 
 		'buildAndTest',
 		'sonar',
-		'run',
+		'jar',
 		'rest',
 		'nexus'
 	]
@@ -36,7 +36,7 @@ def sonar(){
     sh "${sonarhome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
 }
 
-def run(){
+def jar(){
 	sh "nohup bash gradlew bootRun &"
 	sleep 20
 }
