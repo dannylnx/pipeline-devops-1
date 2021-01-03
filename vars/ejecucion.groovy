9d81d1d38592c70b4656f93fefed861623d5f8fd
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2021-01-02 23:38:42 -0300

    cicd

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -4,7 +4,7 @@ def call(){
         agent any
 
         parameters { 
-            choice(name: 'buildtool', choices: ['gradle','maven'], description: 'Elección de herramienta de construcción para aplicación covid')
+            choice(name: 'buildtool', choices: ['maven','gradle'], description: 'Elección de herramienta de construcción para aplicación covid')
             string(name: 'stages', defaultValue: '' , description: 'Escribir stages a ejecutar en formato: stage1;stage2;stage3. Si stage es vacío, se ejecutarán todos los stages.') 
         }
 
@@ -14,11 +14,12 @@ def call(){
                     script{
 
                         figlet params.buildtool
+                        def archivo = (params.buildtool == 'gradle') ? 'build.gradle' : 'pom.xml'
 
-                        if (params.buildtool == 'gradle'){
-                            gradle "${params.stages}" 
+                        if (fileExists(archivo)){
+                            "${params.buildtool}".call("${params.stages}")
                         } else {
-                            maven "${params.stages}"
+                            error "archivo ${archivo} no existe. No se puede construir pipeline basado en ${params.buildtool}"
                         }
 
                     }
