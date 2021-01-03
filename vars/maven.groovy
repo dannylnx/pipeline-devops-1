3dbb176b84a1155bc9b5e8bbb134ef8d227038e6
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2021-01-03 00:05:46 -0300

    cicd

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -43,7 +43,7 @@ def runJar(){
 }
 
 def rest(){
-    sh "curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing"
+    sh "curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing"
 }
 
 def nexusCI(){
