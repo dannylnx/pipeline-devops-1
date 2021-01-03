3f24b704a1c775e5f82b2a5c5f632fe4856a1970
rpinoga <34929141+rpinoga@users.noreply.github.com>
2021-01-02 21:25:25 -0300

    Update ejecucion.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -3,37 +3,33 @@ def call(){
         agent any
         parameters { 
             choice(name: 'buildtool', choices: ['gradle','maven'], description: 'Elección de herramienta')
-        }
-        options {
-            parameters {
-                [$class: 'CascadeChoiceParameter', 
-                    choiceType: 'RADIO', 
-                    description: 'Stages', 
-                    filterLength: 1, 
-                    filterable: false, 
-                    name: 'stages', 
-                    randomName: 'choice-parameter-5631314456178619', 
-                    referencedParameters: 'buildtool', 
+            [$class: 'CascadeChoiceParameter', 
+                choiceType: 'RADIO', 
+                description: 'Stages', 
+                filterLength: 1, 
+                filterable: false, 
+                name: 'stages', 
+                randomName: 'choice-parameter-5631314456178619', 
+                referencedParameters: 'buildtool', 
+                script: [
+                    $class: 'GroovyScript', 
                     script: [
-                        $class: 'GroovyScript', 
-                        script: [
-                            classpath: [], 
-                            sandbox: true, 
-                            script: 
-                                ''' 
-                                    def maven = ['compile','test','jar','runJar','sonar','nexus']
-                                    def gradle = ['buildAndTest','sonar','runJar','rest','nexus']
+                        classpath: [], 
+                        sandbox: true, 
+                        script: 
+                            ''' 
+                                def maven = ['compile','test','jar','runJar','sonar','nexus']
+                                def gradle = ['buildAndTest','sonar','runJar','rest','nexus']
 
-                                    if (buildtool == 'gradle'){
-                                        return gradle
-                                    } else {
-                                        return maven
-                                    }
-                                '''
-                        ]
+                                if (buildtool == 'gradle'){
+                                    return gradle
+                                } else {
+                                    return maven
+                                }
+                            '''
                     ]
                 ]
-            }
+            ]
         }
         stages {
             stage('Pipeline') {
