961216b497d448b4e52b0f3b2ddc1ee8720e115a
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2021-01-06 21:49:08 -0300

    cambio puerto 8082 maven

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -4,7 +4,7 @@ def call(String chosenStages){
 
     def utils  = new test.UtilMethods()
 
-    def pipelineStages = (utils.isCIorCD().contains('ci')) ? ['compile','test','jar','sonar','runJar','rest','nexusCI'] : ['downloadNexus','runDownloadedJar','rest','nexusCD'] 
+    def pipelineStages = (utils.isCIorCD().contains('ci')) ? ['compile','test','jar','sonar','runJar','rest','nexusCI','createRelease'] : ['downloadNexus','runDownloadedJar','rest','nexusCD'] 
 
     def stages = utils.getValidatedStages(chosenStages, pipelineStages)
 
@@ -43,11 +43,26 @@ def runJar(){
 }
 
 def rest(){
-    sh "curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing"
+    sh "curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing"
 }
 
 def nexusCI(){
     nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "build/DevOpsUsach2020-0.0.1.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: "0.0.1-${env.GIT_BRANCH}"]]]  
+}
+
+def createRelease(){
+    def git = new pipeline.git.GitMethods()
+
+    if (git.checkIfBranchExists('release-v1-0-0')){
+        if (git.isBranchUpdated(env.GIT_BRANCH, 'release-v1-0-0')){
+            println 'La rama ya está creada y actualizada contra ' + env.GIT_BRANCH
+        } else {
+            git.deleteBranch('release-v1-0-0')
+            git.createBranch('release-v1-0-0', env.GIT_BRANCH)
+        }
+    } else {
+        git.createBranch('release-v1-0-0', env.GIT_BRANCH)
+    }
 }
 
 def downloadNexus(){
