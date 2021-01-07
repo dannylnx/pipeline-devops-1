dd690257e24303c8a464c1510310e55d1185ef16
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2021-01-06 22:00:57 -0300

    git pull

--- src/pipeline/git/GitMethods.groovy
+++ src/pipeline/git/GitMethods.groovy
@@ -2,21 +2,21 @@ package pipeline.git
 
 
 def checkIfBranchExists(String branch){
-	def output = sh (script: "git ls-remote --heads origin ${branch}", returnStdout: true)
+	def output = sh (script: "git pull; git ls-remote --heads origin ${branch}", returnStdout: true)
 	def respuesta = (!output?.trim()) ? false : true
 
 	return respuesta
 }
 
 def isBranchUpdated(String ramaOrigen, String ramaDestino){
-	def output = sh (script: "git log origin/${ramaDestino}..origin/${ramaOrigen}", returnStdout: true)
+	def output = sh (script: "git pull; git log origin/${ramaDestino}..origin/${ramaOrigen}", returnStdout: true)
 	def respuesta = (!output?.trim()) ? true : false
 
 	return respuesta
 }
 
 def deleteBranch(String branch){
-	sh "git push origin --delete ${branch}"
+	sh "git pull; git push origin --delete ${branch}"
 }
 
 def createBranch(String branch, String ramaOrigen){
