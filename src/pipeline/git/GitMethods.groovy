5a37e0791638c55d1ff7dc729be13ceef8dec29c
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2021-01-06 21:45:14 -0300

    git createRelease

---
+++ src/pipeline/git/GitMethods.groovy
@@ -0,0 +1,32 @@
+package pipeline.git
+
+
+def checkIfBranchExists(String branch){
+	def output = sh (script: "git ls-remote --heads origin ${branch}", returnStdout: true)
+	def respuesta = (!output?.trim()) ? true : false
+
+	return respuesta
+}
+
+def isBranchUpdated(String ramaOrigen, String ramaDestino){
+	def output = sh (script: "git log origin/${ramaDestino}..origin/${ramaOrigen}", returnStdout: true)
+	def respuesta = (!output?.trim()) ? true : false
+
+	return respuesta
+}
+
+def deleteBranch(String branch){
+	sh "git push origin --delete ${branch}"
+}
+
+def createBranch(String branch, String ramaOrigen){
+	sh '''
+		git reset --hard HEAD
+		git pull
+		git checkout '''+ramaOrigen+'''
+		git checkout -b '''+branch+'''
+		git push origin '''+branch+'''
+	'''
+}
+
+return this;
