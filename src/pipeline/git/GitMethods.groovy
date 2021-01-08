21933d7211705a23262f728d23158c79b789f374
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2021-01-07 21:49:22 -0300

    implementando crearRelease()

---
+++ src/pipeline/git/GitMethods.groovy
@@ -0,0 +1,25 @@
+package pipeline.git
+
+def checkIfBranchExists(String branch){
+	def output = sh (script: "git ls-remote --heads origin ${branch}", returnStdout: true)
+	if (output?.trim()) {
+		return true
+	} else {
+		return false
+	}
+}
+
+def deleteBranch(String branch){
+	sh "git push origin --delete ${branch}"
+}
+
+def createBranch(String origin, String newBranch){
+	sh '''
+		git pull 
+		git checkout ${origin}
+		git checkout -b ${newBranch}
+		git push origin ${newBranch}
+	'''
+}
+
+return this;
--- vars/maven.groovy
+++ vars/maven.groovy
@@ -22,7 +22,20 @@ def call(String chosenStages){
 
 def crearRelease(){
     if (env.GIT_BRANCH.contains('develop')){
-        println 'Crear Release'
+        
+        def git = new git.GitMethods()
+
+        if (git.checkIfBranchExists('release-v1-0-0')){
+            println 'La rama existe'
+            git.deleteBranch('release-v1-0-0')
+            println 'Rama eliminada. Se crea nuevamente.'
+            git.createBranch(env.GIT_BRANCH,'release-v1-0-0')
+            println 'Rama creada con éxito.'
+        } else {
+            git.createBranch(env.GIT_BRANCH,'release-v1-0-0')
+            println 'Rama creada con éxito.'
+        }
+
     } else {
         println "La rama ${env.GIT_BRANCH} no corresponde como rama de orígen para la creación de un Release."
     }
