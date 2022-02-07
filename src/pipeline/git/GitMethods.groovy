2699d66eb7b60574a5efe50bf472e76b74a3a98a
Daniel Gavilan <dannylnx@gmail.com>
2022-02-06 21:27:22 -0300

    Modificacion de credenciales

--- src/pipeline/git/GitMethods.groovy
+++ src/pipeline/git/GitMethods.groovy
@@ -10,18 +10,22 @@ def checkIfBranchExists(String branch){
 }
 
 def deleteBranch(String branch){
+	withCredentials([gitUsernamePassword(credentialsId: 'github-tocken',gitToolName: 'git-tool')]) {
 	sh "git push origin --delete ${branch}"
+	}
 }
 
 def createBranch(String origin, String newBranch){
+	withCredentials([gitUsernamePassword(credentialsId: 'github-tocken',gitToolName: 'git-tool')]) {
 	sh '''
-		git fetch -p 
+		git fetch -p
 		git checkout '''+origin+'''; git pull
 		git checkout -b '''+newBranch+'''
 		git push origin '''+newBranch+'''
 		git checkout '''+origin+'''; git pull
 		git branch -d '''+newBranch+'''
 	'''
+	}
 }
 
 return this;
